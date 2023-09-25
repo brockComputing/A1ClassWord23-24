7f41a94f5e5eb54b5ac880179b5578c1fda1289e
Charlie Brown <cbrown@brock.ac.uk>
2023-09-25 15:19:40 +0100

    Add project files.

---
+++ A1ClassWord23-24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1ClassWord23_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
