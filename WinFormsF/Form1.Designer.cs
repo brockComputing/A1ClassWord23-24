d04969cced39d25506d49c83ba71d761d6b35673
Charlie Brown <cbrown@brock.ac.uk>
2024-06-05 14:14:16 +0100

    Moved a button

--- WinFormsF/Form1.Designer.cs
+++ WinFormsF/Form1.Designer.cs
@@ -41,7 +41,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(230, 193);
+            this.button1.Location = new System.Drawing.Point(237, 158);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(179, 180);
             this.button1.TabIndex = 0;
